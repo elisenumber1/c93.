e9d7fb26088c8b82e8496949384c419d95326f2c
ShenhElise <100390294+elisenumber1@users.noreply.github.com>
2022-11-29 20:43:32 -0800

    Add files via upload

--- Kwitter.js
+++ Kwitter.js
@@ -1,5 +1,5 @@
 function addUser() {
 	var username = document.getElementById("user_name").value;
 	localStorage.setItem("user_name",user_name);
-	window.location = "/kwitter_room.html";
+	window.location = "/Kwitter_room.html";
 }
